98e501398cad8d407f913df57d78e14f42041172
samantha lin <samanthalin1992@gmail.com>
2020-07-15 15:56:05 +0800

    all page rwd and menu done

--- js/style.js
+++ js/style.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	$('.hamburger').click(function(){
+		$('.mobile-menu').addClass('menu-show');
+		return false;
+	});
+	$('.close-btn').click(function(){
+		$('.mobile-menu').removeClass('menu-show');
+		return false;
+	});
 	$('#page-ticket .question').click(function(){
 		$(this).toggleClass('question--open');
 		$(this).siblings().removeClass('question--open');
