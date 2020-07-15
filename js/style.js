f32e4f3263e342d0fbb32b1d275ec4e1128acddc
samantha lin <samanthalin1992@gmail.com>
2020-07-15 18:24:54 +0800

    movie intro

--- js/style.js
+++ js/style.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
 		$('.mobile-menu').removeClass('menu-show');
 		return false;
 	});
+	$('#page-program .intro-btn').click(function(){
+		$('body').addClass('overflow');
+		var target = $(this).attr('href');
+		$(target).css('display', 'flex');
+		$(target).find('.window').addClass('animated');
+		return false;
+	});
+	$('.popup button').click(function() {
+		$('.popup').hide();
+		$('body').removeClass('overflow');
+	});
 	$('#page-ticket .question').click(function(){
 		$(this).toggleClass('question--open');
 		$(this).siblings().removeClass('question--open');
